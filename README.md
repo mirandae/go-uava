3b6aba962625ab12cd561d26bfa9c84945eb7011
mirandae <mirae@cs.washington.edu>
2017-08-20 19:51:54 -0500

    Usage example

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # go-uava
 
 Basic pre-conditions checking utilities, similar to a subset of Google Guava (minus the whole generics thing). 
+
+
+Usage Example:
+	
+~~~~
+func fnDef(arg1 string, arg2 bool) {
+    err := check.Preconditions(check.NotNilOrEmpty(arg1), check.Argument(arg2))
+    if err != nil {
+        ...
+    }
+    ...
+}
+
+~~~~
--- check/preconditions.go
+++ check/preconditions.go
@@ -1,22 +1,22 @@
 package check
 
-// checkArgument(boolean)	Checks that the boolean is true. Use for validating arguments to methods.	IllegalArgumentException
-// checkNotNull(T)	Checks that the value is not null. Returns the value directly, so you can use checkNotNull(value) inline.	NullPointerException
-// checkState(boolean)	Checks some state of the object, not dependent on the method arguments. For example, an Iterator might use this to check that next has been called before any call to remove.	IllegalStateException
-// checkElementIndex(int index, int size)	Checks that index is a valid element index into a list, string, or array with the specified size. An element index may range from 0 inclusive to size exclusive. You don't pass the list, string, or array directly; you just pass its size.
-// Returns index.	IndexOutOfBoundsException
-// checkPositionIndex(int index, int size)	Checks that index is a valid position index into a list, string, or array with the specified size. A position index may range from 0 inclusive to size inclusive. You don't pass the list, string, or array directly; you just pass its size.
-// Returns index.	IndexOutOfBoundsException
-// checkPositionIndexes(int start, int end, int size)	Checks that [start, end) is a valid sub range of a list, string, or array with the specified size. Comes with its own error message.
-
+// NotNilOrEmpty
 func NotNilOrEmpty(s string) error {
+	if s == nil || s == "" {
+		return nil
+	}
 	return nil
 }
 
+// Argument checks that the boolean is true
 func Argument(b bool) error {
+	if !b {
+		return nil
+	}
 	return nil
 }
 
+// NotNil checks that the parameter is not nil
 // require i comparable to nil (not a struct)
 func NotNil(i interface{}) error {
 	if i == nil {
@@ -25,15 +25,20 @@ func NotNil(i interface{}) error {
 	return nil
 }
 
+// CheckElementIndex checks that index falls within [0, size)
 func CheckElementIndex(index, size int) error {
+	if index < 0 || index >= size {
+		return nil
+	}
 	return nil
 }
 
+// CheckPositionIndex checks that index falls within [0, size]
 func CheckPositionIndex(index, size int) error {
 	return nil
 }
 
-// Preconditions runs all the individual error checking functions and wraps the failed checks
+// Preconditions runs all the input error checking functions and wraps the failed checks into a single error
 func Preconditions(fns ...func(interface{}) error) error {
 	return nil
 }
